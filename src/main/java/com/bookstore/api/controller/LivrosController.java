37c87c74d10a52a040eb124b7249e4708b9b11a6
JoaoFrancisco11 <joaofrancisco.monteiro11@gmail.com>
2023-08-30 20:56:14 -0300

    :lipstick: feature: Consultar por título e por ISBN

--- src/main/java/com/bookstore/api/controller/LivrosController.java
+++ src/main/java/com/bookstore/api/controller/LivrosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,23 @@ public class LivrosController {
     @GetMapping
     public ResponseEntity<List<Livros>> consultarAutores(){
         return ResponseEntity.status(HttpStatus.OK).body(livrosRepository.findAll());
+    }
+
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Livros>> consultarPorLivro(@PathVariable("titulo") final String titulo){
+        List<Livros> livroEncontrado = livrosRepository.findByTitulo(titulo);
+        if(livroEncontrado.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(livroEncontrado);
+    }
+    @GetMapping("/isbn/{isbn}")
+    public ResponseEntity<List<Livros>> consultaPorIsbn(@PathVariable("isbn") final String isbn){
+        List<Livros> isbnEncontrado = livrosRepository.findByIsbn(isbn);
+        if(isbnEncontrado.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(isbnEncontrado);
     }
 
 }
